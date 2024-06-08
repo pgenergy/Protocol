976c3c97873defce055edc4a4799d7520cb40a37
m43i <36700796+m43i@users.noreply.github.com>
2024-06-08 11:02:18 +0200

    refactor: include all builded proto files

--- build/rust/build.rs
+++ build/rust/build.rs
@@ -3,15 +3,33 @@ use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Collect all .proto files in the proto directory
-    let proto_files: Vec<PathBuf> = glob("proto/*.proto")?
-        .filter_map(Result::ok)
-        .collect();
+    let proto_files: Vec<PathBuf> = glob("proto/*.proto")?.filter_map(Result::ok).collect();
 
     // Convert PathBuf to &str for prost_build::compile_protos
-    let proto_files_str: Vec<&str> = proto_files.iter()
+    let proto_files_str: Vec<&str> = proto_files
+        .iter()
         .map(|path| path.to_str().unwrap())
         .collect();
 
     prost_build::compile_protos(&proto_files_str, &["proto/"])?;
+
+    let out_dir = PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    let mod_file_path = out_dir.join("mod.rs");
+
+    let mut mod_file_content = String::new();
+
+    for entry in std::fs::read_dir(&out_dir)? {
+        let entry = entry?;
+        let path = entry.path();
+
+        if let Some(extension) = path.extension() {
+            if extension == "rs" && entry.file_name() != "mod.rs" {
+                let file_stem = path.file_stem().unwrap().to_str().unwrap();
+                mod_file_content.push_str(&format!("pub mod {};\n", file_stem));
+            }
+        }
+    }
+
+    std::fs::write(mod_file_path, mod_file_content)?;
     Ok(())
 }
--- build/rust/src/lib.rs
+++ build/rust/src/lib.rs
@@ -1,6 +1,3 @@
 pub extern crate prost;
 
-include!(concat!(env!("OUT_DIR"), "/energyleaf.rs"));
-pub mod ml_api {
-    include!(concat!(env!("OUT_DIR"), "/energyleaf_ml.rs"));
-}
+include!(concat!(env!("OUT_DIR"), "/mod.rs"));
